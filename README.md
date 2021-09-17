4f72aad5fc0a6b7250fd0bde05605f2e3bb33015
Luiz Felipe <felipefelizatti215@gmail.com>
2021-09-17 07:00:57 -0300

    Conclusão

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto WatchMe
+
+#### criado diretamente pela rocketseat como um desafio do Ignite, essa é a minha conclusão desse desafio.
+
+- `Desafio`: realizar a componentização de duas áreas originais da aplicação.
+
+- `Ferramentas`: *ReactJS* & *Typescript*
+
+| Como iniciar
+
+1. baixe o repositório e execute o comando yarn na pasta.
+
+2. execute yarn dev e yarn server.
+
+3. abra a aplicação em localhost:8080.
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,55 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { GenreResponseProps, MovieProps } from "../interfaces/global";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface SidebarProps {
+  setMovies: (e: MovieProps[]) => void;
+  setSelectedGenre: (e: GenreResponseProps) => void;
+}
+
+import '../styles/sidebar.scss';
+
+export function SideBar({ setMovies, setSelectedGenre }: SidebarProps) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
